2330b6787b75d12b8a06df6a24e7e79b08b0ccd5
Markus <markus.h.jarvis@gmail.com>
2022-10-29 19:21:02 -0500

    Finalize output logic

--- newbie_solutions/interactive-rating-component/main.js
+++ newbie_solutions/interactive-rating-component/main.js
@@ -1,18 +1,35 @@
+let score = 0;
 
 (function() {
-
-    let score;
-
+    
+    // eventListener for all individual rating scores
     document.querySelectorAll('.rating').forEach(item => {
         item.addEventListener('click', function onClick(event) {
 
             const element = document.getElementById(this.id);
+            setScore(element.innerText);  
             setRatingColors(element);  
         })
     })
 
+    // eventListener for Submit button
+    document.querySelector('.submit-button').addEventListener('click', function onClick(event) {
+
+        const scoreDisplayElement = document.querySelector('.score-select');
+        toggleForm();
+        scoreOutput(scoreDisplayElement);
+
+    })
+
 })()
 
+function getScore() {
+    return score;
+}
+
+function setScore(num) {
+    score = num;
+}
 
 // reset all rating colors; change selected rating color; update score variable
 function setRatingColors(element) {
@@ -23,6 +40,22 @@ function setRatingColors(element) {
 
     element.style.backgroundColor = 'var(--orange)';
     element.style.color = 'var(--white)';
+}
 
-    score = element.innerText;  
+// toggle between the submit review form and the thank you note
+function toggleForm() {
+
+    if(score === 0) { // prevent submittion without score
+        return;
+    }
+
+    const formContainer = document.querySelector('#form-container');
+    formContainer.classList = 'hidden';
+
+    const thankyouContainer = document.querySelector('#thank-you-note');
+    thankyouContainer.classList = 'main-container thankyou-container';
+}
+
+function scoreOutput(scoreDisplayElement) {
+    scoreDisplayElement.innerText = `You selected ${getScore()} out of 5`;
 }
