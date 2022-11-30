0be306c4deba5eb6e6171383370dc1c862452c26
Markus <markus.h.jarvis@gmail.com>
2022-11-30 09:30:50 -0600

    Add eventListener

--- newbie_solutions/pod-request-access-landing-page/main.js
+++ newbie_solutions/pod-request-access-landing-page/main.js
@@ -1,6 +1,27 @@
+const email = doc
+document.querySelector('button');
+
+email.addEventListener("click", (event) => {
+  if(email.validity.typeMismatch) {
+    email.setCustomValidity("Ooops! Please check your email");
+    email.reportValidity();
+  } else {
+    email.setCustomValidity("");
+  }
+})
 
 
 
+/* const email = document.getElementById('email');
+
+email.addEventListener("input", (event) => {
+  if(email.validity.typeMismatch) {
+    email.setCustomValidity("Ooops! Please check your email");
+    email.reportValidity();
+  } else {
+    email.setCustomValidity("");
+  }
+}) */
 
 
 
