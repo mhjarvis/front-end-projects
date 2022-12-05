f56b42a566e59e0bf6748b8a5d2cea456862b483
Markus <markus.h.jarvis@gmail.com>
2022-12-05 10:48:25 -0600

    Add javascript for light button

--- newbie_solutions/article-preview-component-master/main.js
+++ newbie_solutions/article-preview-component-master/main.js
@@ -1,7 +1,10 @@
 const lightButton = document.querySelector('.share-icon-light');
 const lightContainer = document.querySelector('.profile-container-light');
+const darkButton = documnet.querySelector('.share-icon-dark');
+const darkContainer = document.querySelector('.profile-container-dark');
 
 lightButton.addEventListener('click', (event) => {
   event.preventDefault();
   lightContainer.style.display = 'none';
+  darkContainer.style.display = 'flex';
 })
