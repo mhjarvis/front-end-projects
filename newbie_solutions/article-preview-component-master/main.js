4a2a989c69b933b5b8b21dff98b49d39d865f0ca
Markus <markus.h.jarvis@gmail.com>
2022-12-05 10:49:57 -0600

    Add event listener for dark button

--- newbie_solutions/article-preview-component-master/main.js
+++ newbie_solutions/article-preview-component-master/main.js
@@ -1,10 +1,16 @@
 const lightButton = document.querySelector('.share-icon-light');
 const lightContainer = document.querySelector('.profile-container-light');
-const darkButton = documnet.querySelector('.share-icon-dark');
+const darkButton = document.querySelector('.share-icon-dark');
 const darkContainer = document.querySelector('.profile-container-dark');
 
 lightButton.addEventListener('click', (event) => {
   event.preventDefault();
   lightContainer.style.display = 'none';
   darkContainer.style.display = 'flex';
+})
+
+darkButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  darkContainer.style.display = 'none';
+  lightContainer.style.display = 'flex';
 })
